a6c38e1a3c860155ad5068731f1a5efe2f69c561
Ruben <[connor.j.schulz@gmail.com]>
2022-05-12 18:37:27 -0400

    base functionality

---
+++ Develop/README.md
@@ -0,0 +1,34 @@
+# hee
+
+  ## Description
+  hee
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Features](#features)
+  - [How To Contribute](#how to contribute)
+  
+  ## Installation
+  hee
+  
+  ## Usage
+  hee
+  
+  ## Credits
+  hee
+  
+  ## License
+  
+  [![License](https://img.shields.io/badge/license-GNU%20GPLv3-orange)](https://www.gnu.org/licenses/gpl-3.0.en.html)
+    
+  
+  ## Features
+  hee
+  
+  ## How to Contribute
+  hee
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,13 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 const inquirer = require('inquirer');
-const pictures = fs.readdirSync('../screenshots');
+
+const pictures = () => fs.readdirSync('./screenshots');
 
 // TODO: Create an array of questions for user input
 const questions = [
+    
     //Project Title
     {
         type: 'input',
@@ -16,37 +19,73 @@ const questions = [
         type: 'input',
         name: 'description',
         message: 'Give a thorough description of the project'
-    }
+    },
     //Installation
-
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Give a quick rundown of how to install the software'
+    },
     //Usage
-
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Give some insight on how to use the software'
+    },
     //Credits
-
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'State what and who helped create this'
+    },
     //License (can leave empty)
-
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license',
+        choices: [
+            'MIT',
+            'GNU GPLv3',
+            'Attribution 4.0 International'
+        ]
+    },
     //Features
-
+    {
+        type: 'input',
+        name: 'features',
+        message: 'List some of the features'
+    },
     //How To Contribute (default is contributer covenant)
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Give a rundown on how to contribute (default is the contributer covenant)'
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
+
     inquirer
         .prompt(questions)
         .then((answers) => {
-            console.log(answers);
+            fs.writeFile('README.md', generateMarkdown(answers) , err =>{
+                if(err){
+                    console.log(err);
+                }
+                console.log("README created!")
+            })            
         })
         .catch((err) => {
-            console.log(err);
-        })
-        console.log('README created!');
-        console.log(pictures);
-        
-        
+            if(err){
+                console.log(err);
+            }
+        });
+    
+
 }
 
 // Function call to initialize app
